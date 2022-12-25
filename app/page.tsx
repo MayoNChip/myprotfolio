bdb0285dcb06cb933a5a4ddc60d43ffcfe575518
Ido Cohen <Idoic44@gmail.com>
2022-12-25 11:45:22 +0200

    changed cover image

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import coverPicture from "../public/main_cover.png";
 
 function page() {
   return (
@@ -9,13 +10,15 @@ function page() {
         <div className="flex flex-col items-center">
           <Image
             className="rounded-full"
-            src="https://avatars.githubusercontent.com/u/94110636?s=400&u=66151f36a76602b53d888f41d5a8c311c8da4623&v=4"
+            // src="https://avatars.githubusercontent.com/u/94110636?s=400&u=66151f36a76602b53d888f41d5a8c311c8da4623&v=4"
+            src={coverPicture}
             alt="Profile Picture"
             width={350}
             height={350}
           />
           <h1 className="text-6xl font-light text-secondary text-left">
-            Hi. I'm Ido.<br></br> A Developer.
+            Hi. I'm Ido.
+            <br /> A Developer.
           </h1>
         </div>
         <div className="w-1/2 h-full flex flex-coll items-center">
