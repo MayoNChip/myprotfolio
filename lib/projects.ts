383c1fafcfb188004a9f0e7ea60abd373046d3e9
Ido Cohen <Idoic44@gmail.com>
2023-02-04 00:51:25 +0200

    added project tags that will be displayed + cleanup

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,23 +1,26 @@
 import { z } from "zod";
 import guruImage from "../public/guru.png";
-
-const projects2 = z.object({
-  title: z.string(),
-  desription: z.string(),
-  url: z.string().url(),
-  image: z.any(),
-});
+import ChatAppImage from "../public/angularTodos.png";
 
 export const projects = [
   {
     title: "guru - Grocery shop",
     description: "some description",
+    tags: ["React", "ChakraUI", "Frontend"],
     url: "http://www.google.com",
     image: guruImage,
   },
   {
+    title: "do! - Todo's App",
+    description: "To-do app.",
+    tags: ["AngularJS", "TailwindCSS", "Frontend"],
+    url: "http://www.google.com",
+    image: ChatAppImage,
+  },
+  {
     title: "Pet Project",
-    description: "some description",
+    description: "Pet Adoption.",
+    tags: ["React", "Frontend", "Backend", "ChakraUI", "MongoDB", "Express"],
     url: "http://www.google.com",
     image: guruImage,
   },
