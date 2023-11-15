6da417b550bad636d73b275fd3dd5530cdf020a4
Ido Cohen <Idoic44@gmail.com>
2023-11-15 20:26:51 +0200

    fix fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,27 @@
 "use client";
-import React, { useContext, useState } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
-import ThemeProvider, {
-  GlobalContext,
-  InitialContext,
-} from "../context/GlobalContext";
+import ThemeProvider from "../context/GlobalContext";
 import "../styles/globals.css";
 
 interface Props {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 function layout({ children }: Props) {
-  return (
-    <html lang="en">
-      <head />
-      <body>
-        <ThemeProvider>
-          <div className="flex flex-col w-full">
-            <Navbar />
-            {children}
-          </div>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head />
+			<body>
+				<ThemeProvider>
+					<div className="flex flex-col w-full">
+						<Navbar />
+						{children}
+					</div>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
 
 export default layout;
