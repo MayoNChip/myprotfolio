41afd758bb74dddfc6a2518e6a84631c1fd6f217
Ido Cohen <94110636+MayoNChip@users.noreply.github.com>
2023-11-18 14:00:49 +0200

    email template change

--- app/contactme/nodeMailer.ts
+++ app/contactme/nodeMailer.ts
@@ -1,39 +1,45 @@
 "use server";
 
-import { createTransport } from "nodemailer";
+import { createTransport, SendMailOptions } from "nodemailer";
 import { z, ZodError } from "zod";
 
 const FormValidation = z.object({
-	from: z.string().email(),
-	message: z.string().min(8, "Minimun 8 characters"),
+  from: z.string().email(),
+  message: z.string().min(8, "Minimun 8 characters"),
 });
 
 export async function sendEmail(prevState: any, formData: FormData) {
-	console.log(formData.get("from"));
-	const transporter = createTransport({
-		host: "smtp.gmail.com",
-		port: 465,
-		secure: true,
-		auth: {
-			user: "idocodev@gmail.com",
-			pass: process.env.GOOGLE_APP_PASSWORD,
-		},
-	});
-	try {
-		const data = FormValidation.parse({
-			from: formData.get("from"),
-			message: formData.get("message"),
-		});
-		const emailOptions = {
-			from: data.from,
-			to: "idocodev@gmail.com",
-			subject: "New Potential Empoloyer!",
-			text: data.message,
-		};
+  console.log(formData.get("from"));
+  const transporter = createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "idocodev@gmail.com",
+      pass: process.env.GOOGLE_APP_PASSWORD,
+    },
+  });
+  try {
+    const data = FormValidation.parse({
+      from: formData.get("from"),
+      message: formData.get("message"),
+    });
+    const emailOptions: SendMailOptions = {
+      from: data.from,
+      to: "idocodev@gmail.com",
+      subject: "New Potential Empoloyer!",
+      //   text: data.message,
+      html: `<body> <h1>Sender:</h1><div>${data.from}</div>
+	  <br/>
+	  <h1>Message:</h1>
+	  <br/>
+	  <div>${data.message}</div> </body>`,
+    };
 
-		transporter.sendMail(emailOptions);
-		return { message: "message sent" };
-	} catch (error) {
-		return { message: "message failed to send" };
-	}
+    transporter.sendMail(emailOptions);
+    return { message: "message sent" };
+  } catch (error) {
+    console.log(error);
+    return { message: "message failed to send" };
+  }
 }
